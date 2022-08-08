e018b1550ddfc1e65d5b8ae2878448e181df1e59
SAM BHU <SAM BHU@DESKTOP-5MMOUET>
2022-08-08 17:45:13 +0545

    Add project files.

---
+++ FILE IO_in _C_Sharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FILE_IO_in__C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream f = new FileStream("C:\\Users\\SAM BHU\\Desktop\\new\\shambhu.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            for(int i = 0; i <= 20; i++)
+            {
+                f.WriteByte((byte)i);
+            }
+
+            f.Position = 0;
+            for(int i = 0; i <= 20; i++)
+            {
+                Console.WriteLine(f.ReadByte() + " ");
+            }
+            f.Close();
+            Console.ReadKey();
+        }
+    }
+}
